033165314bb53fbfbc9a769a30554ed867190f75
Robbie <rthomlinson03@gmail.com>
2025-01-09 10:50:38 -0800

    added backend auth and user entity

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './auth.local.strategy';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { jwtSecret } from './secrets';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
